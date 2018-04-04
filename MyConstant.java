f54be24cd87944a12564ade8ba9c8fb4b9c6861a
linghan <linghan.ca@gmail.com>
2018-04-03 22:41:11 -0400

    enum

---
+++ MyConstant.java
@@ -0,0 +1,28 @@
+package ca.hockey.wet;
+
+public class MyConstant {
+
+		final public static String CONS = "Y";
+
+
+		public enum AppType {
+			TRADITIONAL((short) 26),
+			UNTRADITIONAL((short) 67);
+
+			private final short appTypeId;
+
+			AppType(short appTypeId) {
+				this.appTypeId = appTypeId;
+			}
+
+			public short getAppTypeId() {
+				return appTypeId;
+			}
+
+		}
+
+		public enum ENTITY_GET_DEFAULT_PARAMETER {
+			RelatedIndicator,
+			UnrelatedIndicator
+		}
+}
