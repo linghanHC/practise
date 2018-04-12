2009ae4cf9f0e5e055344a61a65307704c96dad9
Hubert Jin <hubert.jin@kodacloud.com>
2018-04-11 21:17:26 -0400

    hello from Hubert

--- wet-cdts/src/main/java/test/merge/HelloWorld.java
+++ wet-cdts/src/main/java/test/merge/HelloWorld.java
@@ -3,8 +3,7 @@ package test.merge;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World from Hubert");
     }
 
 }
