7dd93486aabdcdfa4b01f5b22466d2097c4540de
linghan <linghan.ca@gmail.com>
2018-04-11 21:43:53 -0400

    hello again

--- wet-cdts/src/main/java/test/merge/HelloWorld.java
+++ wet-cdts/src/main/java/test/merge/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         // Prints "Hello, World" to the terminal window.
     	// another comment
         System.out.println("Hello, World from Ling");
+        System.out.println("Hello, World from Ling one more time");
     }
 
 }
